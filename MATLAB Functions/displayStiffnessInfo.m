74df628d4e75144a34c99f9ca0996e4015cc7eb2
csharp14 <104353572+csharp14@users.noreply.github.com>
2024-03-12 09:21:41 +0000

    Removed unnecessary functions

--- MATLAB Functions/displayStiffnessInfo.m
+++
@@ -1,9 +0,0 @@
-function displayStiffnessInfo()
-disp(['x st. dev. is ' num2str(stdev_Gauss_lab(1),3) ' nm'])
-disp(['y st. dev. is ' num2str(stdev_Gauss_lab(2),3) ' nm'])
-disp(['z st. dev. is ' num2str(stdev_Gauss_lab(3),3) ' nm'])
-disp(['x stiffness is ' num2str(k_Gauss_lab(1),3) ' N/m'])
-disp(['y stiffness is ' num2str(k_Gauss_lab(2),3) ' N/m'])
-disp(['z stiffness is ' num2str(k_Gauss_lab(3),3) ' N/m'])
-disp(['Volume of ellipsoid is ' num2str(vol_Gauss_lab,8) ' nm^3'])
-end
